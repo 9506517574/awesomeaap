d0bf69ad5bfee77bc2150ef6f448811cf54ae903
9506517574 <114390462+9506517574@users.noreply.github.com>
2022-11-03 21:47:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<p>I am learning React. My life is getting better</p>,document.getElementById("root"));
+ReactDOM.render(<p>I am learning React. My life is getting better.</p>,document.getElementById("root"));
